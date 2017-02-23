99e076b1607742ed1c00fa81a4e454ba40c8b838
Zach Cowell <zach.cowell@gmail.com>
2017-02-22 23:05:03 -0500

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import _ from 'underscore';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div className="app">
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
